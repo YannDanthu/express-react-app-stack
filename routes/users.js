9f5b7009572e0dda4c520388719c2f5ee969cb55
Yann <yann@danthu.com>
2017-09-07 17:26:25 +0200

    add the create-react-app part

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  res.json([{
+  	id: 1,
+  	firstname: 'sampleuser1',
+    lastname: 'Expressor',
+  }, {
+  	id: 2,
+  	firstname: 'sampleuser2',
+    lastname: 'Reactor',
+  }]);
 });
 
 module.exports = router;
